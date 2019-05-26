1aa8aa31f24c2a2d2361249a144db521b365f34d
Javiera Vergara <javiera.vergaran@utem.cl>
2019-05-26 18:08:36 -0400

    Se añade una base para el menú

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 def combinarListas(l1, l2):
     aux = l1
     if type(l2) == list:
@@ -17,7 +20,8 @@ def conexionesConVacio(lista, nodo):
     for tupla in lista:
         if(nodo == tupla[0] and tupla[1] == ""):
             nodosVacios = combinarListas(nodosVacios, tupla[2])
-            nodosVacios = combinarListas(nodosVacios, conexionesConVacio(lista, tupla[2]))
+            # En caso de multiples conexiones vacías:
+            # nodosVacios = combinarListas(nodosVacios, conexionesConVacio(lista, tupla[2]))
     return nodosVacios
 
 def procesarNodo(lista, alfabeto, nodo):
@@ -43,6 +47,40 @@ def procesarNodo(lista, alfabeto, nodo):
                         diccionario[c] = combinarListas(diccionario[c], e)
     return diccionario
 
+def opciones():
+    print("\n\tMenu principal")
+    print("(1) ")
+    print("(2)")
+    print("(3)")
+    print("(4)")
+    print("(0) Salir")
+
+def menu():
+    while True:
+        opciones()
+        opcion = input("Ingrese una opcion: ")
+
+        if opcion == 1:
+            print("Opcion 1")
+        elif opcion == 2:
+            print("Opcion 2")
+        elif opcion == 3:
+            print("Opcion 3")
+        elif opcion == 4:
+            print("Opcion 4")
+        elif opcion == 0:
+            break
+        else:
+            print("Opcion invalida")
+
+def main():
+    print("Grafos y lenguajes formales\n21041|INFB8061-1\n\nTrabajo 2: Automata\n")
+    print("Integrantes:\n")
+    print("- Felipe Flores Vivanco\n- Andres Mella\n- Jorge Verdugo Chacon\n- Javiera Vergara Navarro")
+    menu()
+
+
+##Datos de prueba:
 
 E=["a","b"]
 L=[("Q0","a","Q2"),
@@ -52,10 +90,8 @@ L=[("Q0","a","Q2"),
    ("Q1","b","Q1"),
    ("Q2","a","Q0"),
    ("Q3","a","Q1"),
-   ("Q4","a","Q4"),
-   ("Q1", "", "Q8")]
-
-K=["Q0","Q1","Q2","Q3","Q4", "Q8"]
+   ("Q4","a","Q4")]
+K=["Q0","Q1","Q2","Q3","Q4"]
 S=["Q0"]
 F=["Q4"]
 
@@ -69,3 +105,6 @@ for nodo in K:
     aux=procesarNodo(L, E, nodo)
     tabla[nodo] = aux
     print(aux)
+
+#Llamada del main, se comenta por mientras uwu
+#main()
